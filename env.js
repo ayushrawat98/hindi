a5ab358b730ae0cdb3d41871e20c3026793c7507
ayushrawat98 <aayushrawat0@gmail.com>
2026-07-18 11:59:26 +0530

    Env udpate

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv()
 
 export const configuration = {
-	NODE_ENV : process.env.NODE_ENV.trim() || "development",
-	PORT : process.env.PORT.trim() || 3000,
-	FFMPEG : process.env.FFMPEG.trim() || "ffmpeg"
+	NODE_ENV : process.env.NODE_ENV?.trim() || "development",
+	PORT : process.env.PORT?.trim() || 3000,
+	FFMPEG : process.env.FFMPEG?.trim() || "ffmpeg"
 }
