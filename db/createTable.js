8b553448b751660ec3fc584b81131b272387d1a3
ayushrawat98 <aayushrawat0@gmail.com>
2026-08-08 03:06:47 +0530

    Remove board part 1

--- db/createTable.js
+++ db/createTable.js
@@ -2,60 +2,70 @@ import instance from "./db.js";
 
 const migrate = instance.db.transaction(() => {
 
-	
-  // 1. Create the new table
-  instance.db.exec(`
-    CREATE TABLE IF NOT EXISTS files_new (
+
+
+
+
+
+	// 1. Create the new table
+	instance.db.exec(`
+    CREATE TABLE IF NOT EXISTS posts_new (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      path TEXT NOT NULL,
-      type TEXT,
-      height INTEGER default 0,
-      width INTEGER default 0,
-	  size INTEGER,
-      status TEXT,
-      created_at TEXT
+      parent_id INTEGER DEFAULT NULL REFERENCES posts(id) ON DELETE CASCADE,
+      name TEXT,
+      title TEXT,
+	content TEXT,
+					file_id INTEGER REFERENCES files(id),
+					ip TEXT,
+					replies INTEGER DEFAULT 0,
+					created_at TEXT,
+					updated_at TEXT
     );
   `);
 
-  // 2. Copy data from old table
-  // height and width are initialized to NULL
-  instance.db.exec(`
-    INSERT INTO files_new (
+	// 2. Copy data from old table
+	// height and width are initialized to NULL
+	instance.db.exec(`
+    INSERT INTO posts_new (
       id,
-      path,
-      type,
-      height,
-      width,
-	  size,
-      status,
-      created_at
+      parent_id,
+      name,
+      title,
+      content,
+	  file_id,
+      ip,
+	  replies,
+      created_at,
+	  updated_at
     )
     SELECT
       id,
-      path,
-      type,
-      0,
-      0,
-	  size,
-      status,
-      created_at
-    FROM files;
+      parent_id,
+      name,
+      title,
+      content,
+	  file_id,
+      ip,
+	  replies,
+      created_at,
+	  updated_at
+    FROM posts;
   `);
 
-  // 3. Drop the old table
-  instance.db.exec(`DROP TABLE files;`);
+	// 3. Drop the old table
+	instance.db.exec(`DROP TABLE posts;`);
 
-  // 4. Rename the new table
-  instance.db.exec(`ALTER TABLE files_new RENAME TO files;`);
+	// 4. Rename the new table
+	instance.db.exec(`ALTER TABLE posts_new RENAME TO posts;`);
 
- 
+
 });
 
 try {
 	instance.db.exec("PRAGMA foreign_keys = OFF;");
-  migrate();
-   instance.db.exec("PRAGMA foreign_keys = ON;");
-  console.log("Migration completed successfully.");
+	migrate();
+	instance.db.exec("PRAGMA foreign_keys = ON;");
+	console.log("Migration completed successfully.");
 } catch (err) {
-  console.error("Migration failed:", err);
+	console.error("Migration failed:", err);
 }
