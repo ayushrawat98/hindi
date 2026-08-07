a68e8ed996920e412edb787953c69165953f0396
ayushrawat98 <aayushrawat0@gmail.com>
2026-08-08 02:14:43 +0530

    Revert "Image hxw update 2"
    This reverts commit dfa5d604727d2784721f4dfc3ec2bec8fb20bed6.

--- db/db.js
+++ db/db.js
@@ -46,6 +46,8 @@ class DB {
 					id integer primary key autoincrement,
 					path text not null,
 					type text, 
+      				height INTEGER default 0,
+      				width INTEGER default 0,
 					size integer,
 					status text,
   					created_at text
@@ -81,7 +83,7 @@ class DB {
 
 			getFileWithStatus : this.db.prepare('select * from files where status = ?'),
 			updateFileStatus : this.db.prepare('update files set status = ? where id = ?'),
-			// updateFileHeightAndWidth : this.db.prepare("UPDATE files SET height = ?, width = ? WHERE id = ?"),
+			updateFileHeightAndWidth : this.db.prepare("UPDATE files SET height = ?, width = ? WHERE id = ?"),
 
 			getHotPosts: this.db.prepare('select t.id, t.title, t.content, t.name, t.created_at, t.replies, f.path as file_path, f.type as file_type, f.status as file_status from posts t left join files f on t.file_id = f.id where t.parent_id is null order by t.updated_at desc limit 100'),
 			getNewPosts: this.db.prepare('select t.id, t.title, t.content, t.name, t.created_at, t.replies, f.path as file_path, f.type as file_type, f.status as file_status from posts t left join files f on t.file_id = f.id where t.parent_id is null order by t.created_at desc limit 100'),
